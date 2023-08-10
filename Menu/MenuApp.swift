243157f6acc0e80c6eacc5e0c0bc84bbc77ae301
Peter <1pete3learning@gmail.com>
2023-08-10 03:11:18 -0700

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Admin on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
